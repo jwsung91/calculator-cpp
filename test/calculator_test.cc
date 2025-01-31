874830dfa8a2d367d702990af4287ea7d6cd3c28
Jinwoo Sung <123284056+jwsung91@users.noreply.github.com>
2025-01-31 23:21:55 +0900

    feat: make test case (#6)
    * feat: make test case
    
    * feat: test divide by zero

--- test/calculator_test.cc
+++ test/calculator_test.cc
@@ -1,11 +1,39 @@
 #include <gtest/gtest.h>
 #include "calculator.hpp"
 
-TEST(CalculatorTest, BasicAssertions)
+class CalculatorTest : public ::testing::Test
 {
-    Calculator calc;
-    EXPECT_EQ(calc.Plus(1, 1), 2);
-    EXPECT_EQ(calc.Minus(2, 1), 1);
-    EXPECT_EQ(calc.Multiply(2, 2), 4);
-    EXPECT_EQ(calc.Divide(4, 2), 2);
+protected:
+    Calculator Calc;
+};
+
+TEST_F(CalculatorTest, Addition)
+{
+    EXPECT_EQ(Calc.Plus(1, 1), 2);
+}
+
+TEST_F(CalculatorTest, Subtraction)
+{
+    EXPECT_EQ(Calc.Minus(2, 1), 1);
+}
+
+TEST_F(CalculatorTest, Multiplication)
+{
+    EXPECT_EQ(Calc.Multiply(2, 2), 4);
+}
+
+TEST_F(CalculatorTest, Division)
+{
+    EXPECT_EQ(Calc.Divide(4, 2), 2);
+}
+
+TEST_F(CalculatorTest, DivideByZero)
+{
+    EXPECT_THROW(Calc.Divide(4, 0), std::invalid_argument);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
